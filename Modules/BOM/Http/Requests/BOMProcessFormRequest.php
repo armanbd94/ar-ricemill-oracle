715fe8fc0d0c8a516e4e78bca27de1171c54ca16
Mohammad Arman <arman.ctgbd1394@gmail.com>
2021-12-12 18:48:21 +0600

    BOM Process Store Done

--- Modules/BOM/Http/Requests/BOMProcessFormRequest.php
+++ Modules/BOM/Http/Requests/BOMProcessFormRequest.php
@@ -2,7 +2,7 @@
 
 namespace Modules\BOM\Http\Requests;
 
-use Illuminate\Foundation\Http\FormRequest;
+use App\Http\Requests\FormRequest;
 
 class BOMProcessFormRequest extends FormRequest
 {
@@ -14,7 +14,49 @@ class BOMProcessFormRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'memo_no'              => ['required'],
+            'batch_id'             => ['required'],
+            'process_number'       => ['required'],
+            'to_product_id'        => ['required'],
+            'to_site_id'           => ['required'],
+            'to_location_id'       => ['required'],
+            'from_product_id'      => ['required'],
+            'product_particular'   => ['required'],
+            'product_per_unit_qty' => ['required','gt:0'],
+            'product_required_qty' => ['required','gt:0'],
+            'bag_site_id'          => ['required'],
+            'bag_location_id'      => ['required'],
+            'bag_id'               => ['required'],
+            'bag_particular'       => ['required'],
+            'bag_per_unit_qty'     => ['required','gt:0'],
+            'bag_required_qty'     => ['required','gt:0'],
+            'total_rice_qty'       => ['required','gt:0'],
+            'total_bag_qty'        => ['required','gt:0'],
+            'process_date'         => ['required'],
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'batch_id.required'       => 'This field is required',
+            'process_number.required' => 'This field is required',
+            'to_product_id.required'  => 'This field is required',
+            'to_site_id'              => 'This field is required',
+            'to_location_id'          => 'This field is required',
+            'from_product_id'         => 'This field is required',
+            'product_particular'      => 'This field is required',
+            'product_per_unit_qty'    => 'This field is required',
+            'product_required_qty'    => 'This field is required',
+            'bag_site_id'             => 'This field is required',
+            'bag_location_id'         => 'This field is required',
+            'bag_id'                  => 'This field is required',
+            'bag_particular'          => 'This field is required',
+            'bag_per_unit_qty'        => 'This field is required',
+            'bag_required_qty'        => 'This field is required',
+            'total_rice_qty'          => 'This field is required',
+            'total_bag_qty'           => 'This field is required',
+            'process_date'            => 'This field is required',
         ];
     }
 
